fdaf1649d05a8eceaaa7e5c493dc965e7760b350
thaisc98 <thaisc98@hotmail.es>
2024-11-22 15:39:18 -0400

    [NEW] impl signup and login

--- src/store/auth/auth.reducer.js
+++ src/store/auth/auth.reducer.js
@@ -2,26 +2,45 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
-export const login = createAsyncThunk('auth/login', async (credentials) => {
+export const login = createAsyncThunk("auth/login", async (credentials) => {
+  console.log("credentials", credentials);
   const response = await fetch(`${API_BASE_URL}/Authentication/login`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(credentials),
   });
 
   if (!response.ok) {
-    throw new Error('Failed to log in');
+    throw new Error("Failed to log in");
   }
 
   const data = await response.json();
-  return data.token; 
+  return data.token;
+});
+
+//  sign-up
+export const signUp = createAsyncThunk('auth/signUp', async (userDetails) => {
+  const response = await fetch(`${API_BASE_URL}/Authentication/signin`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userDetails),
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to sign up');
+  }
+
+  const data = await response.json();
+  return data;
 });
 
 const INITIAL_STATE = {
   isAuthenticated: !!localStorage.getItem("authToken"),
-  status: 'idle',
+  status: "idle",
   error: null,
 };
 
@@ -34,18 +53,29 @@ const authSlice = createSlice({
       state.isAuthenticated = false;
     },
   },
-   extraReducers: (builder) => {
+  extraReducers: (builder) => {
     builder
       .addCase(login.pending, (state) => {
-        state.status = 'loading';
+        state.status = "loading";
         state.error = null;
       })
       .addCase(login.fulfilled, (state, action) => {
-        localStorage.setItem('authToken', action.payload);
+        localStorage.setItem("authToken", action.payload);
         state.isAuthenticated = true;
-        state.status = 'succeeded';
+        state.status = "succeeded";
       })
       .addCase(login.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
+      .addCase(signUp.pending, (state) => {
+        state.status = 'loading';
+        state.error = null;
+      })
+      .addCase(signUp.fulfilled, (state) => {
+        state.status = 'succeeded';
+      })
+      .addCase(signUp.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.error.message;
       });
